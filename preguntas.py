7bc05f3e252fe318b132f45f38641c1dab654e35
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-11-29 17:43:24 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -197,35 +197,41 @@ def pregunta_03():
     """
 
     # Importe ColumnTransformer
+    from sklearn.compose import ColumnTransformer
+
     # Importe SVC
+    from sklearn.svm import SVC
+
     # Importe OneHotEncoder
+    from sklearn.preprocessing import OneHotEncoder
+
     # Importe Pipeline
-    from ____ import ____
+    from sklearn.pipeline import Pipeline
 
     # Cargue las variables.
-    X_train, _, y_train, _ = pregunta_02()
+    X_train, X_test, y_train, y_test = pregunta_02()
 
     # Cree un objeto ColumnTransformer que aplique OneHotEncoder a las columnas
     # tipo texto. Use make_column_selector para seleccionar las columnas. Las
     # columnas numéricas no deben ser transformadas.
-    columnTransformer = make_column_transformer(
+    columnTransformer = ColumnTransformer(
         (
-            ____(),
-            ____(____=____),
+            OneHotEncoder(),
+            make_column_selector(dtype_include="object"),
         ),
-        remainder=____,
+        remainder="passthrough",
     )
 
     # Cree un pipeline que contenga el columnTransformer y el modelo SVC.
-    pipeline = ____(
+    pipeline = Pipeline(
         steps=[
-            ("____", ____),
-            ("____", ____),
+            ("columnTransformer", columnTransformer),
+            ("svc", SVC()),
         ],
     )
 
     # Entrene el pipeline con los datos de entrenamiento.
-    ____.____(____, ____)
+    pipeline.fit(X_train, y_trainx)
 
     # # Retorne el pipeline entrenado
     return pipeline
@@ -237,7 +243,7 @@ def pregunta_04():
     """
 
     # Importe confusion_matrix
-    from ____ import ____
+    from sklearn.metrics import confusion_matrix
 
     # Obtenga el pipeline de la pregunta 3.
     pipeline = pregunta_03()
@@ -246,14 +252,14 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
-    cfm_train = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_train = confusion_matrix(
+        y_true=y_train,
+        y_pred=pipeline.predict(X_trainx),
     )
 
-    cfm_test = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_test = confusion_matrix(
+        y_true=y_test,
+        y_pred=pipeline.predict(X_test),
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
